60eb5797cc3eac32a28f1249fea0a5ee48049e04
manjugdr <153181318+manjugdr@users.noreply.github.com>
2024-12-06 16:56:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "19.0.0"
+  version         = "19.0.0" # Ensure you're using the correct version
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
   vpc_id          = var.vpc_id
-  subnet_ids      = var.subnets
+  subnet_ids      = var.subnet_ids
 
   managed_node_groups = {
-    eks_nodes = {
-      desired_capacity = var.node_desired_capacity
-      max_capacity     = var.node_max_capacity
-      min_capacity     = var.node_min_capacity
+    default = {
       instance_types   = [var.node_instance_type]
+      desired_capacity = var.node_desired_capacity
+      min_size         = var.node_min_capacity
+      max_size         = var.node_max_capacity
     }
   }
 
