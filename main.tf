a6c28035f5d08638ad80d1ad6a7930a8972213ec
manjugdr <153181318+manjugdr@users.noreply.github.com>
2024-12-06 16:53:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,17 @@
-# AWS Provider
-provider "aws" {
-  region = var.aws_region
-}
-
-# EKS Cluster Module
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "19.0.0"  # Adjust to the latest version
+  version         = "19.0.0"
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
-  subnets         = var.subnets
   vpc_id          = var.vpc_id
+  subnet_ids      = var.subnets
 
-  node_groups = {
+  managed_node_groups = {
     eks_nodes = {
       desired_capacity = var.node_desired_capacity
       max_capacity     = var.node_max_capacity
       min_capacity     = var.node_min_capacity
-
-      instance_type = var.node_instance_type
+      instance_types   = [var.node_instance_type]
     }
   }
 
