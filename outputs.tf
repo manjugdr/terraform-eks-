64be99729dcebf5b743411a97060388180446349
manjugdr <153181318+manjugdr@users.noreply.github.com>
2024-12-06 16:57:53 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,24 @@
 output "eks_cluster_id" {
-  description = "EKS Cluster ID"
+  description = "The EKS cluster ID"
   value       = module.eks.cluster_id
 }
 
 output "eks_cluster_endpoint" {
-  description = "EKS Cluster endpoint"
+  description = "The endpoint for the EKS cluster"
   value       = module.eks.cluster_endpoint
 }
 
 output "eks_cluster_version" {
-  description = "EKS Cluster version"
+  description = "The Kubernetes version for the EKS cluster"
   value       = module.eks.cluster_version
+}
+
+output "eks_node_group_arns" {
+  description = "ARNs of the EKS managed node groups"
+  value       = module.eks.managed_node_group_arns
+}
+
+output "eks_node_group_names" {
+  description = "Names of the EKS managed node groups"
+  value       = module.eks.managed_node_group_names
 }
